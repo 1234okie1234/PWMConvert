9dd7200b6e82a67c38805f72f0d2cc6675a759b7
DSGoose <1234okie1234@gmail.com>
2023-08-11 11:50:37 -0700

    Added , in millions, quality of life change

--- script.js
+++ script.js
@@ -9,19 +9,24 @@ function convert() {
             destinationValue = 'Invalid input! Enter a non-negative number.';
         } else {
             switch (conversionType) {
-                case 'dollars_to_gold': destinationValue = sourceValue * 1000000 / 3.30; break;
-                case 'gold_to_dollars': destinationValue = sourceValue * 3.30 / 1000000; break;
-                case 'dollars_to_unbound': destinationValue = sourceValue * 6000 / 100; break;
-                case 'unbound_to_dollars': destinationValue = sourceValue * 100 / 6000; break;
-                case 'dollars_to_bound': destinationValue = sourceValue * 12000 / 100; break;
                 case 'bound_to_dollars': destinationValue = sourceValue * 100 / 12000; break;
-                case 'gold_to_bound': destinationValue = sourceValue * 396 / 1000000; break;
                 case 'bound_to_gold': destinationValue = sourceValue * 1000000 / 396; break;
-                case 'gold_to_unbound': destinationValue = sourceValue * 6000 / 3.30; break;
-                case 'unbound_to_gold': destinationValue = sourceValue * 3.30 / 6000; break;
-                case 'unbound_to_bound': destinationValue = sourceValue * 2; break;
                 case 'bound_to_unbound': destinationValue = sourceValue / 2; break;
+                case 'dollars_to_bound': destinationValue = sourceValue * 12000 / 100; break;
+                case 'dollars_to_gold': destinationValue = sourceValue * 1000000 / 3.30; break;
+                case 'dollars_to_unbound': destinationValue = sourceValue * 6000 / 100; break;
+                case 'gold_to_bound': destinationValue = sourceValue * 396 / 1000000; break;
+                case 'gold_to_dollars': destinationValue = sourceValue * 3.30 / 1000000; break;
+                case 'gold_to_unbound': destinationValue = sourceValue * 6000 / 3.30; break;
+                case 'unbound_to_bound': destinationValue = sourceValue * 2; break;
+                case 'unbound_to_dollars': destinationValue = sourceValue * 100 / 6000; break;
+                case 'unbound_to_gold': destinationValue = sourceValue * 3.30 / 6000; break;
             }
+        }
+
+        // Format the result with commas
+        if (typeof destinationValue === 'number') {
+            destinationValue = destinationValue.toLocaleString('en-US');
         }
     } catch (e) {
         destinationValue = 'Invalid input! Enter a number.';
