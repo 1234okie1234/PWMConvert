dbb37f82f8f4d0646a4285e86c6cdbdcbffe8f1a
DSGoose <1234okie1234@gmail.com>
2023-08-11 12:25:22 -0700

    Corrected conversion rate

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ function convert() {
             switch (conversionType) {
                 case 'bound_to_dollars': destinationValue = sourceValue * 100 / 12000; break;
                 case 'bound_to_gold': destinationValue = sourceValue * 1000000 / 396; break;
-                case 'bound_to_unbound': destinationValue = sourceValue / 2; break;
+                case 'bound_to_unbound': destinationValue = sourceValue * 6000 / 12000; break;
                 case 'dollars_to_bound': destinationValue = sourceValue * 12000 / 100; break;
-                case 'dollars_to_gold': destinationValue = sourceValue * 1000000 / 3.30; break;
+                case 'dollars_to_gold': destinationValue = sourceValue * 1000000 / 3.3; break;
                 case 'dollars_to_unbound': destinationValue = sourceValue * 6000 / 100; break;
                 case 'gold_to_bound': destinationValue = sourceValue * 396 / 1000000; break;
-                case 'gold_to_dollars': destinationValue = sourceValue * 3.30 / 1000000; break;
-                case 'gold_to_unbound': destinationValue = sourceValue * 6000 / 3.30; break;
-                case 'unbound_to_bound': destinationValue = sourceValue * 2; break;
+                case 'gold_to_dollars': destinationValue = sourceValue * 3.3 / 1000000; break;
+                case 'gold_to_unbound': destinationValue = sourceValue * 6000 / 3.3; break;
+                case 'unbound_to_bound': destinationValue = sourceValue * 12000 / 6000; break;
                 case 'unbound_to_dollars': destinationValue = sourceValue * 100 / 6000; break;
-                case 'unbound_to_gold': destinationValue = sourceValue * 3.30 / 6000; break;
+                case 'unbound_to_gold': destinationValue = sourceValue * 3.3 / 6000; break;
             }
         }
 
