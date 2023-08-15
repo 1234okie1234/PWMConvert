64fae191d1c2d9a9944b13cfe7cb9e53dbd2dea2
DSGoose <1234okie1234@gmail.com>
2023-08-15 10:07:29 -0700

    Added custom gold rate feature

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function convert() {
     var sourceValue = document.getElementById('sourceValue').value;
     var conversionType = document.getElementById('conversion').value;
+    var customRateValue = document.getElementById('customRate').value || 3.33;
     var destinationValue;
 
     try {
         sourceValue = parseFloat(sourceValue);
+        customRateValue = parseFloat(customRateValue);
         if (sourceValue < 0) {
             destinationValue = 'Invalid input! Enter a non-negative number.';
         } else {
@@ -13,10 +15,10 @@ function convert() {
                 case 'bound_to_gold': destinationValue = sourceValue * 1000000 / 396; break;
                 case 'bound_to_unbound': destinationValue = sourceValue * 6000 / 12000; break;
                 case 'dollars_to_bound': destinationValue = sourceValue * 12000 / 100; break;
-                case 'dollars_to_gold': destinationValue = sourceValue * 1000000 / 3.3; break;
+                case 'dollars_to_gold': destinationValue = sourceValue * 1000000 / customRateValue; break;
                 case 'dollars_to_unbound': destinationValue = sourceValue * 6000 / 100; break;
                 case 'gold_to_bound': destinationValue = sourceValue * 396 / 1000000; break;
-                case 'gold_to_dollars': destinationValue = sourceValue * 3.3 / 1000000; break;
+                case 'gold_to_dollars': destinationValue = sourceValue * customRateValue / 1000000; break;
                 case 'gold_to_unbound': destinationValue = sourceValue * 198 / 1000000; break;
                 case 'unbound_to_bound': destinationValue = sourceValue * 12000 / 6000; break;
                 case 'unbound_to_dollars': destinationValue = sourceValue * 100 / 6000; break;
